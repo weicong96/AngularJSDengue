9353618a76784ff045f878ac677ee51f502f5223
Wei Cong <twentyeightbytes@gmail.com>
2015-06-15 22:32:50 -0400

    Added add park functionality

--- app/app.module.js
+++ app/app.module.js
@@ -1,2 +1,9 @@
 var app = angular.module("dengueApp", ["dengueRouter","ngResource"]);
 app.constant("API_URL", "http://weicong.chickenkiller.com:3000/");
+app.factory("Park",["API_URL","$resource",function(API_URL, $resource){
+	return $resource(API_URL+"park/:id", {id : "@_id"}, {
+		update:{
+			method: "PUT"
+		}
+	});
+}]);
--- app/components/AddPark/AddParkController.js
+++ app/components/AddPark/AddParkController.js
@@ -1,5 +1,10 @@
 var app = angular.module("dengueApp",["ui.router"]);
 
-app.controller("AddParkController",["$scope","$state",function($scope,$state){
-    $scope.message = "Hello";
+app.controller("AddParkController",["$scope","$state","Park",function($scope,$state,Park){
+	$scope.park = {};
+	$scope.submitForm = function(){
+		Park.save($scope.park, function(){
+			console.log("Saved");
+		});
+	}
 }]);
